4860a52f342c15976ef3da47d281e99b85052439
Amos Langat <amos.langat@student.moringaschool.com>
2022-10-17 11:42:30 +0300

    complete assignment

---
+++ app.rb
@@ -0,0 +1,5 @@
+puts "Hello World!" # outputs strings on the terminal with line breaks
+
+print "Pass this test, please." #print outputs strings without line breaks
+
+p [1, 2, 3] # this calls .inspect method
